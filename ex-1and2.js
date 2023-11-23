73de36a08498458e334f3a37d0ca9a328406a5b5
peerawet <peerawet1996@gmail.com>
2023-11-23 13:59:44 +0700

    Variable Creation and Variable Reassignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
-//Exercise #1: Variable Creation
-//Start coding here
+// Exercise #1: Variable Creation
+// Start coding here
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const netFoodPrice = 15000;
+const numberOfOpenRestaurants = 1500;
+
+// Exercise #2: Variable Reassignment
+// Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netFoodPrice = 5000; // 1.5
+numberOfOpenRestaurants = 2500;
