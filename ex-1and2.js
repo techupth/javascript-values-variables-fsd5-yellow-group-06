c160a38f44767f8175b9160f1fb33e94c32ffa0b
Am <arphasirisubb@gmail.com>
2023-11-23 14:05:35 +0700

    submit assignment js

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let name = "Karter";
+let surName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const netPrice = 15000;
+const restaurantOpen = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd 4 Meeker, Colorado(CO), 81641";
+
+netPrice = 5000;
+restaurantOpen = 2500;
